9fbd9e84994010dd82aa701c1caf37a9b6791b7e
SPaganoDoval <dovalsonia@gmail.com>
2023-03-10 20:30:45 +0000

    Data User

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +18,38 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+# class Evento(db.Model):
+#             id = db.Column(db.Integer, primary_key=True)
+#             name = db.Column(db.String(80), unique=False, nullable=False)
+#             id_participante = db.Column(db.String(80), unique=False, nullable=False)
+#             categoria = db.Column(db.String(80), unique=False, nullable=False)
+#             fecha = db.Column(db.String(120), unique=True, nullable=False)
+#             descripcion = db.Column(db.String(80), unique=False, nullable=False)
+#             lugar = db.Column(db.String(80), unique=False, nullable=False)
+#             dificultad = db.Column(db.String(80), unique=False, nullable=False)
+            
+
+    # def __repr__(self):
+    #     return f'<Evento {self.id}>'
+
+    # def serialize(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #         "id_participante": self.participante,
+    #         "categoria": self.categoria,
+    #         "fecha": self.fecha,
+    #         "descripcion": self.descripcion,
+    #         "lugar": self.lugar,
+    #         "dificultad": self.dificultad,
+
+
+
+            # do not serialize the password, its a security breach
+    #    }
